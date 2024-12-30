2dc63b77413837913c20da74372d86b8cd873287
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-12-30 14:51:21 -0800

    style: Improve SearchTerm clear button appearance

--- src/components/SearchTerm/SearchTerm.jsx
+++ src/components/SearchTerm/SearchTerm.jsx
@@ -1,25 +1,32 @@
-import styles from "./SearchTerm.module.css";
+import React from 'react';
+import styles from './SearchTerm.module.css';
+import { useDispatch } from 'react-redux';
+import { clearSearchTerm } from './searchTermSlice';
+import { FaTimes } from 'react-icons/fa';
 
 const SearchTerm = ({ searchTerm, onSearchChange }) => {
-  return (
-    <div className={styles.searchContainer}>
-      <input
-        type="text"
-        placeholder="Search Pokemon..."
-        value={searchTerm}
-        onChange={onSearchChange}
-        className={styles.searchInput}
-      />
-      {searchTerm && (
-        <button
-          className={styles.clearButton}
-          onClick={() => onSearchChange({ target: { value: "" } })}
-        >
-          x
-        </button>
-      )}
-    </div>
-  );
-};
+    const dispatch = useDispatch();
+
+    const handleClearSearchTerm = () => {
+        dispatch(clearSearchTerm());
+    }
+
+    return (
+      <div className={styles.searchContainer}>
+          <input
+              type="text"
+              placeholder='Search Pokemon...'
+              value={searchTerm}
+              onChange={onSearchChange}
+              className={styles.searchInput}
+          />
+          {searchTerm && (
+              <button className={styles.clearButton} onClick={handleClearSearchTerm}>
+                  <FaTimes /> {/* Use the icon component */}
+              </button>
+          )}
+      </div>
+  )
+}
 
 export default SearchTerm;
