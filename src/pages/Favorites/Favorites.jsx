442270f6b2fa100cb744041abb1c95e515a3dd6b
drohack <drohack@gmail.com>
2025-01-08 20:56:11 -0600

    Fix Favorites checkboxes

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -15,6 +15,8 @@ function Favorites() {
     // Get selected types from Redux state
     const selectedTypes = useSelector((state) => state.typeFilter?.selectedTypes || []);
     // Get selected legendaries from Redux state
+    const selectedPseudoLegendaries = useSelector((state) => state.pseudoLegendaryFilter?.selectedPseudoLegendaries || []);
+    // Get selected legendaries from Redux state
     const selectedLegendaries = useSelector((state) => state.legendaryFilter?.selectedLegendaries || []);
     // Get selected starters from Redux state
     const selectedStarters = useSelector((state) => state.starterFilter?.selectedStarters || []);
@@ -25,13 +27,15 @@ function Favorites() {
     const starterIds = Object.values(StartersAndEvolutions).map(starter => starter.id);
 
     // Convert PseudoLegendaries, SubLegendaries, Legendaries, and Mythical objects to arrays of IDs
-    const pseudoLegendaryIds = Object.values(PseudoLegendaries).map(pseudoLegendary => pseudoLegendary.id);
     const subLegendaryIds = Object.values(SubLegendaries).map(subLegendary => subLegendary.id);
     const legendaryIds = Object.values(Legendaries).map(legendary => legendary.id);
     const mythicalIds = Object.values(Mythical).map(mythical => mythical.id);
 
     // Combine all legendary-related IDs into a single array
-    const allLegendaryIds = [...pseudoLegendaryIds, ...subLegendaryIds, ...legendaryIds, ...mythicalIds];
+    const allLegendaryIds = [...subLegendaryIds, ...legendaryIds, ...mythicalIds];
+
+    // Check if any selected legendaries are in the SubLegendaries list
+    const combinedLegendariesPseudoStarterIds = [ ...selectedLegendaries, ...selectedPseudoLegendaries, ...selectedStarters ];
 
 
     const handleClearFavorites = () => {
@@ -49,14 +53,9 @@ function Favorites() {
         );
 
         // Check if the Pokémon's ID is in the selected legendaries list (or if none are selected)
-        const legendaryMatches =
-            selectedLegendaries.length === 0 ||
-            selectedLegendaries.includes(p.id);
-
-        // Check if the Pokémon's ID is in the selected starters list (or if none are selected)
-        const starterMatches =
-            selectedStarters.length === 0 ||
-            selectedStarters.includes(p.id);
+        const combinedLegendaryPseudoStarterMatches = 
+            combinedLegendariesPseudoStarterIds.length === 0 || 
+            combinedLegendariesPseudoStarterIds.includes(p.id);
 
         // Check if the Pokémon's ID is in the StartersAndEvolutions list if "Starters" is in selectedExclusions
         const isStarterExcluded =
@@ -74,7 +73,7 @@ function Favorites() {
                 p.types.some((typeObj) => typeObj.type.name === excludedType)
             );
 
-        return typesMatch && legendaryMatches && starterMatches && !isStarterExcluded && !isLegendaryExcluded && !isTypeExcluded;
+        return typesMatch && combinedLegendaryPseudoStarterMatches && !isStarterExcluded && !isLegendaryExcluded && !isTypeExcluded;
     });
 
     return (
